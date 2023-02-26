d69012ae8be3c909301aba47cf189b38c725208a
fadialjohari0 <fadialjohari0@sociumtech.com>
2023-02-26 23:14:22 +0300

    Final 1

--- src/views/calculator/Calculator.jsx
+++ src/views/calculator/Calculator.jsx
@@ -1,11 +1,15 @@
 import React, { useContext, useState } from "react";
-import styles from "./Calculator.module.css";
-import { CalculatorContext } from "../../context/CalculatorContext";
+import * as math from "mathjs";
 
+import { CalculatorContext } from "../../context/CalculatorContext";
+import CalculatorButtons from "./CalculatorButtons";
+
+import styles from "./Calculator.module.css";
 const Calculator = () => {
   const [displayValue, setDisplayValue] = useState("0");
   const [operator, setOperator] = useState("");
   const [storedValue, setStoredValue] = useState("");
+  const [error, setError] = useState(false);
 
   const {
     handleCalculation,
@@ -13,26 +17,36 @@ const Calculator = () => {
     setIsHistorySelected,
     isHistorySelected,
   } = useContext(CalculatorContext);
-  const executeCalculation = (num1, num2, operator) => {
-    switch (operator) {
-      case "+":
-        return num1 + num2;
-      case "-":
-        return num1 - num2;
-      case "*":
-        return num1 * num2;
-      case "/":
-        return num1 / num2;
-      case "%":
-        return num1 % num2;
-      case "+/-":
-        return num1 * -1;
-      case ".":
-        return parseFloat(num1 + "." + num2);
-      default:
-        return 0;
-    }
-  };
+
+  const buttons = [
+    { value: "C", className: styles.operator, onClick: () => handleClick("C") },
+    {
+      value: "+/-",
+      className: styles.operator,
+      onClick: () => handleClick("+/-"),
+    },
+    { value: "%", className: styles.operator, onClick: () => handleClick("%") },
+    { value: "/", className: styles.operator, onClick: () => handleClick("/") },
+    { value: "7", className: styles.number, onClick: () => handleClick("7") },
+    { value: "8", className: styles.number, onClick: () => handleClick("8") },
+    { value: "9", className: styles.number, onClick: () => handleClick("9") },
+    { value: "x", className: styles.operator, onClick: () => handleClick("*") },
+    { value: "4", className: styles.number, onClick: () => handleClick("4") },
+    { value: "5", className: styles.number, onClick: () => handleClick("5") },
+    { value: "6", className: styles.number, onClick: () => handleClick("6") },
+    { value: "-", className: styles.operator, onClick: () => handleClick("-") },
+    { value: "1", className: styles.number, onClick: () => handleClick("1") },
+    { value: "2", className: styles.number, onClick: () => handleClick("2") },
+    { value: "3", className: styles.number, onClick: () => handleClick("3") },
+    { value: "+", className: styles.operator, onClick: () => handleClick("+") },
+    {
+      value: "0",
+      className: `${styles.number} ${styles.zeroNumber}`,
+      onClick: () => handleClick("0"),
+    },
+    { value: ".", className: styles.operator, onClick: () => handleClick(".") },
+    { value: "=", className: styles.operator, onClick: () => handleClick("=") },
+  ];
 
   const handleClick = (char) => {
     switch (char) {
@@ -57,37 +71,49 @@ const Calculator = () => {
       case "*":
       case "/":
       case "%":
-      case "+/-":
-      case ".":
         setOperator(char);
         setStoredValue(displayValue);
         setDisplayValue("0");
+        break;
+      case "+/-":
+        setDisplayValue((parseFloat(displayValue) * -1).toString());
+        break;
+      case ".":
+        if (!displayValue.includes(".")) {
+          setDisplayValue(displayValue + ".");
+        }
         break;
       case "C":
         setDisplayValue("0");
         setOperator("");
         setStoredValue("");
+        setError(false);
         break;
       case "=":
-        let [num1, num2] = [parseFloat(storedValue), parseFloat(displayValue)];
-        let result = executeCalculation(num1, num2, operator);
-        setDisplayValue(result.toFixed(1));
-        setOperator("");
-        setStoredValue("");
-        if (isHistorySelected) {
-          num1 = parseFloat(selectedHistory);
-          result = executeCalculation(num1, num2, operator);
-          handleCalculation(
-            `${num1} ${operator} ${num2} = ${result.toFixed(1)}`
-          );
-          setIsHistorySelected(false);
+        let num1 = parseFloat(storedValue);
+        try {
+          let num2 = parseFloat(displayValue);
+          let result = math.evaluate(`${num1} ${operator} ${num2}`);
           setDisplayValue(result.toFixed(1));
-        } else {
-          handleCalculation(
-            `${num1} ${operator} ${num2} = ${result.toFixed(1)}`
-          );
+          setOperator("");
+          setStoredValue("");
+          if (isHistorySelected) {
+            num1 = parseFloat(selectedHistory);
+            result = math.evaluate(`${num1} ${operator} ${num2}`);
+            handleCalculation(
+              `${num1} ${operator} ${num2} = ${result.toFixed(1)}`
+            );
+            setIsHistorySelected(false);
+            setDisplayValue(result.toFixed(1));
+          } else {
+            handleCalculation(
+              `${num1} ${operator} ${num2} = ${result.toFixed(1)}`
+            );
+          }
+          setError(false);
+        } catch (err) {
+          setError(true);
         }
-
         break;
       default:
         break;
@@ -97,71 +123,23 @@ const Calculator = () => {
   return (
     <div className={styles.calculator}>
       <div className={styles.result}>
+        {error ? (
+          <span className={styles.errorMessage}>Second value unavailable</span>
+        ) : (
+          ""
+        )}
         <p className={styles.resultValue}>
           {isHistorySelected ? selectedHistory : displayValue}
         </p>
       </div>
       <div className={styles.calcButtons}>
-        <button className={styles.operator} onClick={() => handleClick("C")}>
-          C
-        </button>
-        <button className={styles.operator} onClick={() => handleClick("+/-")}>
-          +/-
-        </button>
-        <button className={styles.operator} onClick={() => handleClick("%")}>
-          %
-        </button>
-        <button className={styles.operator} onClick={() => handleClick("/")}>
-          /
-        </button>
-        <button className={styles.number} onClick={() => handleClick("7")}>
-          7
-        </button>
-        <button className={styles.number} onClick={() => handleClick("8")}>
-          8
-        </button>
-        <button className={styles.number} onClick={() => handleClick("9")}>
-          9
-        </button>
-        <button className={styles.operator} onClick={() => handleClick("*")}>
-          x
-        </button>
-        <button className={styles.number} onClick={() => handleClick("4")}>
-          4
-        </button>
-        <button className={styles.number} onClick={() => handleClick("5")}>
-          5
-        </button>
-        <button className={styles.number} onClick={() => handleClick("6")}>
-          6
-        </button>
-        <button className={styles.operator} onClick={() => handleClick("-")}>
-          -
-        </button>
-        <button className={styles.number} onClick={() => handleClick("1")}>
-          1
-        </button>
-        <button className={styles.number} onClick={() => handleClick("2")}>
-          2
-        </button>
-        <button className={styles.number} onClick={() => handleClick("3")}>
-          3
-        </button>
-        <button className={styles.operator} onClick={() => handleClick("+")}>
-          +
-        </button>
-        <button
-          className={`${styles.number} ${styles.zeroNumber}`}
-          onClick={() => handleClick("0")}
-        >
-          0
-        </button>
-        <button className={styles.operator} onClick={() => handleClick(".")}>
-          .
-        </button>
-        <button className={styles.operator} onClick={() => handleClick("=")}>
-          =
-        </button>
+        {buttons.map((button) => (
+          <CalculatorButtons
+            value={button.value}
+            className={button.className}
+            onClick={button.onClick}
+          />
+        ))}
       </div>
     </div>
   );
